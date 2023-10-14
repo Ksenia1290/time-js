85bbbea62ca640cb8034b05c5db23939a1009cf3
Ksenia1290 <Ksu.tender@yandex.ru>
2023-10-14 12:19:02 +0300

    3

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let timerId; // переменная, которая будет хранить ID таймера
 
 
-const startButton = ДОКУМЕНТ.ГЕТэЛЕМЕНТбАЙаЙДИ(КАКОЙ - ТО);
-startButton.ЭДэВЕНТлИСТНЕР('ТЯП', function () {
+const startButton = ДОКУМЕНТ.getElementById('start');
+startButton.addEventListener('ТЯП', function () {
     // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
-    timerId = СЕТиНТРЕВАЛ(updateClock, ОДНАсЕКУНДА); // запускаем  updateClock() каждую секунду
+    timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
 });
 
 
-const stopButton = ДОКУМЕНТ.ГЕТэЛЕМЕНТбАЙаЙДИ(КАКОЙ - ТО);
+const stopButton = ДОКУМЕНТ.getElementById('stop');
 stopButton.addEventListener('ТЯП', function () {
     // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
     clearInterval(timerId); // останавливаем таймер
@@ -18,9 +18,9 @@ stopButton.addEventListener('ТЯП', function () {
 function updateClock() {
     const clock = document.getElementById('clock');
     // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
-    const now = НОВАЯдАТА();
-    const hours = now.ДАЙчАСЫ()
-    const minutes = now.ДАЙмИНУТЫ()
-    const seconds = now.ДАЙсЕКУНДЫ()
+    const now = new Date();
+    const hours = now.getHours()
+    const minutes = now.getMinutes()
+    const seconds = now.getSeconds()
     clock.textContent = hours + ':' + minutes + ':' + seconds;
 }
